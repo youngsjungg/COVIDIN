da69ac445893e157d7e22b1f1c93fbfaa234ad49
jungs <youngsjungg@gmail.com>
2020-11-22 14:47:45 +0900

    spinner

--- app/src/main/java/com/example/covidin/xmlpasing.java
+++ app/src/main/java/com/example/covidin/xmlpasing.java
@@ -1,67 +1,38 @@
-package com.example.covidin;
-
-import android.app.Activity;
-import android.os.Bundle;
-import android.view.View;
-import android.widget.AdapterView;
-import android.widget.EditText;
-import android.widget.TextView;
-import org.xmlpull.v1.XmlPullParser;
-import org.xmlpull.v1.XmlPullParserFactory;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.net.URL;
-import java.net.URLEncoder;
-import android.widget.ArrayAdapter;
-import android.widget.Spinner;
-
-
-public class MainActivity extends Activity {
-
-
-    Spinner spinner;
-    EditText edit;
-    TextView text;
-
-    XmlPullParser xpp;
-    String key = "INtNC54XZqdLzdO%2BaPP5sO9ZR3%2FgW1z7IbGhd6uPOuYaTmB5iYtZKJLRme9rlEEn23GBSkUNSIJUZYQ%2FygATHw%3D%3D";
-
-
-    String data;
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
-
-        spinner = findViewById(R.id.spinner);
-
-        ArrayAdapter monthAdapter = ArrayAdapter.createFromResource(this, R.array.sido, android.R.layout.simple_spinner_dropdown_item);
-        monthAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        spinner.setAdapter(monthAdapter); //어댑터에 연결해줍니다.
-
-
-        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
-            @Override
-            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-            } //이 오버라이드 메소드에서 position은 몇번째 값이 클릭됬는지 알 수 있습니다.
-            //getItemAtPosition(position)를 통해서 해당 값을 받아올수있습니다.
-
-            @Override
-            public void onNothingSelected(AdapterView<?> parent) {
-            }
-        });
-    }
-}
-//}
+//package com.example.covidin;
+//
+//import android.app.Activity;
+//import android.os.Bundle;
+//import android.view.View;
+//import android.widget.EditText;
+//import android.widget.TextView;
+//import org.xmlpull.v1.XmlPullParser;
+//import org.xmlpull.v1.XmlPullParserFactory;
+//import java.io.InputStream;
+//import java.io.InputStreamReader;
+//import java.net.URL;
+//import java.net.URLEncoder;
+//
+//public class MainActivity extends Activity {
+//
+//    EditText edit;
+//    TextView text;
+//
+//    XmlPullParser xpp;
+//    String key="INtNC54XZqdLzdO%2BaPP5sO9ZR3%2FgW1z7IbGhd6uPOuYaTmB5iYtZKJLRme9rlEEn23GBSkUNSIJUZYQ%2FygATHw%3D%3D";
+//
+//
+//    String data;
+//
+//    @Override
+//    protected void onCreate(Bundle savedInstanceState) {
+//        super.onCreate(savedInstanceState);
+//        setContentView(R.layout.activity_main);
+//
 //        edit= (EditText)findViewById(R.id.edit);
 //        text= (TextView)findViewById(R.id.result);
 //    }
 //
-
-
-    //Button을 클릭했을 때 자동으로 호출되는 callback method....
+//    //Button을 클릭했을 때 자동으로 호출되는 callback method....
 //    public void mOnClick(View v){
 //        switch( v.getId() ){
 //            case R.id.button:
