f86ec31fc005d63c1bd025b9187effe8e8f9415c
jungs <youngsjungg@gmail.com>
2020-12-02 22:52:55 +0900

    co

--- app/src/main/java/com/example/covidin/serva.java
+++
@@ -1,4 +0,0 @@
-package com.example.covidin;
-
-public class serva {
-}
---
+++ app/src/main/java/com/example/covidin/servaActivity.java
@@ -0,0 +1,13 @@
+package com.example.covidin;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class servaActivity extends Activity {
+
+    public void onCreate (Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.id.activity_serva);
+    }
+}
