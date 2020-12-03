fdcecba6fca4cd564f5d129c4e760a24f3c249d7
jungs <youngsjungg@gmail.com>
2020-12-03 10:32:17 +0900

    co

--- app/src/main/java/com/example/covidin/MainActivity.java
+++ app/src/main/java/com/example/covidin/MainActivity.java
@@ -30,57 +30,30 @@ public class MainActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_main);}
+
+        public void onClick (View view) {
+            startActivity(new Intent("servaActivity"));
+        }
 
 
-        spinner = (Spinner)findViewById(R.id.spinner);
 
-        //input array data
-        final ArrayList<String> list = new ArrayList<>();
-        list.add("도시를 선택하세요");
-        list.add("경남");
-        list.add("경북");
-        list.add("제주");
+        final TextView textView = (TextView)findViewById(R.id.text2);
+        Spinner spinner = (Spinner)findViewById(R.id.spinner);
 
-        String[] list2 = new String[2];
-        list2[0] = xmlpasing.java;
-        list2[1] = "하세요";
-
-        //using ArrayAdapter
-        ArrayAdapter spinnerAdapter;
-        spinnerAdapter = new ArrayAdapter(this, R.layout.support_simple_spinner_dropdown_item, list2);
-        spinner.setAdapter(spinnerAdapter);
-
-        //event listener
+       
         spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             @Override
-            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-                Toast.makeText(MainActivity.this,"선택된 아이템 : "+spinner.getItemAtPosition(position),Toast.LENGTH_SHORT).show();
+            public void onItemSelected Object parent;
+        (AdapterView<?> parent, View view, int position, long id) {
             }
-
             @Override
-            public void onNothingSelected(AdapterView<?> parent) {
-
-            }
+            public void onNothingSelected(AdapterView<?> parent) { }
         });
     }
 }
 
 
-//        ArrayAdapter monthAdapter = ArrayAdapter.createFromResource(this, R.array.sido, android.R.layout.simple_spinner_dropdown_item);
-//        monthAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-//        spinner.setAdapter(monthAdapter); //어댑터에 연결해줍니다.
-//
-//
-//        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
-//            @Override
-//            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-//            }
-//            @Override
-//            public void onNothingSelected(AdapterView<?> parent) { }
-//        });
-//    }
-//}
 
 
      
--- app/src/main/java/com/example/covidin/servaActivity.java
+++ app/src/main/java/com/example/covidin/servaActivity.java
@@ -8,6 +8,6 @@ public class servaActivity extends Activity {
     public void onCreate (Bundle savedInstanceState) {
 
         super.onCreate(savedInstanceState);
-        setContentView(R.id.activity_serva);
+        setContentView(R.layout.activity_serva);
     }
 }
