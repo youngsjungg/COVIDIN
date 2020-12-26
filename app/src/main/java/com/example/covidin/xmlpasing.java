7367b29c1ec5e70d05e2d19a859af3f1e58d6471
jungs <youngsjungg@gmail.com>
2020-12-26 15:56:46 +0900

    paxml

--- app/src/main/java/com/example/covidin/xmlpasing.java
+++ app/src/main/java/com/example/covidin/xmlpasing.java
@@ -1,9 +1,13 @@
 //package com.example.covidin;
 //
 //import android.app.Activity;
+//import android.content.Intent;
 //import android.os.Bundle;
 //import android.view.View;
+//import android.widget.AdapterView;
+//import android.widget.Button;
 //import android.widget.EditText;
+//import android.widget.SearchView;
 //import android.widget.TextView;
 //import org.xmlpull.v1.XmlPullParser;
 //import org.xmlpull.v1.XmlPullParserFactory;
@@ -11,14 +15,33 @@
 //import java.io.InputStreamReader;
 //import java.net.URL;
 //import java.net.URLEncoder;
+//import java.nio.file.attribute.AclEntry;
+//import java.util.ArrayList;
+//
+//import android.widget.ArrayAdapter;
+//import android.widget.Spinner;
+//import android.widget.Toast;
+//
+//import com.example.covidin.R;
+////
+////import okhttp3.ResponseBody;
+////import retrofit2.Call;
+////import retrofit2.Retrofit;
+//////import retrofit2.converter.gson.GsonConverterFactory;
+////import retrofit2.converter.simplexml.SimpleXmlConverterFactory;
+////import retrofit2.http.GET;
+////
+//////import static retrofit2.converter.gson.GsonConverterFactory.*;
+//
 //
 //public class MainActivity extends Activity {
 //
-//    EditText edit;
+//
+//    SearchView searchView;
 //    TextView text;
 //
 //    XmlPullParser xpp;
-//    String key="INtNC54XZqdLzdO%2BaPP5sO9ZR3%2FgW1z7IbGhd6uPOuYaTmB5iYtZKJLRme9rlEEn23GBSkUNSIJUZYQ%2FygATHw%3D%3D";
+//    String key = "INtNC54XZqdLzdO%2BaPP5sO9ZR3%2FgW1z7IbGhd6uPOuYaTmB5iYtZKJLRme9rlEEn23GBSkUNSIJUZYQ%2FygATHw%3D%3D";
 //
 //
 //    String data;
@@ -28,14 +51,16 @@
 //        super.onCreate(savedInstanceState);
 //        setContentView(R.layout.activity_main);
 //
-//        edit= (EditText)findViewById(R.id.edit);
-//        text= (TextView)findViewById(R.id.result);
+//        searchView = (SearchView) findViewById((R.id.search_view));
+//        text = (TextView) findViewById(R.id.result);
 //    }
 //
+//
+//
 //    //Button을 클릭했을 때 자동으로 호출되는 callback method....
-//    public void mOnClick(View v){
-//        switch( v.getId() ){
-//            case R.id.button:
+//    public void mOnClick(View v) {
+//        switch (v.getId()) {
+//            case R.id.btn_sv:
 //
 //                //Android 4.0 이상 부터는 네트워크를 이용할 때 반드시 Thread 사용해야 함
 //                new Thread(new Runnable() {
@@ -43,7 +68,7 @@
 //                    @Override
 //                    public void run() {
 //                        // TODO Auto-generated method stub
-//                        data= getXmlData();//아래 메소드를 호출하여 XML data를 파싱해서 String 객체로 얻어오기
+//                        data = getXmlData();//아래 메소드를 호출하여 XML data를 파싱해서 String 객체로 얻어오기
 //
 //                        //UI Thread(Main Thread)를 제외한 어떤 Thread도 화면을 변경할 수 없기때문에
 //                        //runOnUiThread()를 이용하여 UI Thread가 TextView 글씨 변경하도록 함
@@ -62,108 +87,109 @@
 //
 //
 //    //XmlPullParser를 이용하여 Naver 에서 제공하는 OpenAPI XML 파일 파싱하기(parsing)
-//    String getXmlData(){
+//    String getXmlData() {
 //
-//        StringBuffer buffer=new StringBuffer();
+//        StringBuffer buffer = new StringBuffer();
+////        String str= searchView.getText().toString();//EditText에 작성된 Text얻어오기
+////        String location = URLEncoder.encode(str);
 //
-//        String str= edit.getText().toString();//EditText에 작성된 Text얻어오기
-//        String location = URLEncoder.encode(str);//한글의 경우 인식이 안되기에 utf-8 방식으로 encoding..
-//
-//
-////        String queryUrl="http://openapi.data.go.kr/openapi/service/rest/Covid19/getCovid19SidoInfStateJson?" +
-////                "serviceKey=INtNC54XZqdLzdO%2BaPP5sO9ZR3%2FgW1z7IbGhd6uPOuYaTmB5iYtZKJLRme9rlEEn23GBSkUNSIJUZYQ%2FygATHw%3D%3D" +
-////                "&pageNo=1&numOfRows=10 &startCreateDt=20201121&endCreateDt=20201121";
+////        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+////                                              @Override
+////                                              public boolean onQueryTextSubmit(String query) {
+////                                                  return false;
+////                                              }
 ////
+////                                              @Override
+////                                              public boolean onQueryTextChange(String newText) {
+////                                                  return false;
+////                                              }
 //
-//        String queryUrl="http://openapi.data.go.kr/openapi/service/rest/Covid19/getCovid19SidoInfStateJson?" +
+//
+//
+////
+//////        String queryUrl="http://openapi.data.go.kr/openapi/service/rest/Covid19/getCovid19SidoInfStateJson?" +
+//////                "serviceKey=INtNC54XZqdLzdO%2BaPP5sO9ZR3%2FgW1z7IbGhd6uPOuYaTmB5iYtZKJLRme9rlEEn23GBSkUNSIJUZYQ%2FygATHw%3D%3D" +
+//////                "&pageNo=1&numOfRows=10 &startCreateDt=20201121&endCreateDt=20201121";
+//////
+//
+//        String queryUrl = "http://openapi.data.go.kr/openapi/service/rest/Covid19/getCovid19SidoInfStateJson?" +
 //                "serviceKey=INtNC54XZqdLzdO%2BaPP5sO9ZR3%2FgW1z7IbGhd6uPOuYaTmB5iYtZKJLRme9rlEEn23GBSkUNSIJUZYQ%2FygATHw%3D%3D" +
 //                "&pageNo=1&numOfRows=10";
 //
 //        try {
-//            URL url= new URL(queryUrl);//문자열로 된 요청 url을 URL 객체로 생성.
-//            InputStream is= url.openStream(); //url위치로 입력스트림 연결
+//            URL url = new URL(queryUrl);//문자열로 된 요청 url을 URL 객체로 생성.
+//            InputStream is = url.openStream(); //url위치로 입력스트림 연결
 //
-//            XmlPullParserFactory factory= XmlPullParserFactory.newInstance();
-//            XmlPullParser xpp= factory.newPullParser();
-//            xpp.setInput( new InputStreamReader(is, "UTF-8") ); //inputstream 으로부터 xml 입력받기
+//            XmlPullParserFactory factory = XmlPullParserFactory.newInstance();
+//            XmlPullParser xpp = factory.newPullParser();
+//            xpp.setInput(new InputStreamReader(is, "UTF-8")); //inputstream 으로부터 xml 입력받기
 //
 //            String tag;
 //
 //            xpp.next();
-//            int eventType= xpp.getEventType();
+//            int eventType = xpp.getEventType();
 //
-//            while( eventType != XmlPullParser.END_DOCUMENT ){
-//                switch( eventType ){
+//            while (eventType != XmlPullParser.END_DOCUMENT) {
+//                switch (eventType) {
 //                    case XmlPullParser.START_DOCUMENT:
 //                        buffer.append("파싱 시작...\n\n");
 //                        break;
 //
 //                    case XmlPullParser.START_TAG:
-//                        tag= xpp.getName();//테그 이름 얻어오기
+//                        tag = xpp.getName();//테그 이름 얻어오기
 //
-//                        if(tag.equals("item")) ;// 첫번째 검색결과
-//                        else if(tag.equals("createDt")){
+//                        if (tag.equals("item")) ;// 첫번째 검색결과
+//                        else if (tag.equals("createDt")) {
 //                            buffer.append("등록일시분 : ");
 //                            xpp.next();
 //                            buffer.append(xpp.getText());//title 요소의 TEXT 읽어와서 문자열버퍼에 추가
 //                            buffer.append("\n"); //줄바꿈 문자 추가
-//                        }
-//                        else if(tag.equals("deathCnt")){
+//                        } else if (tag.equals("deathCnt")) {
 //                            buffer.append("사망자  : ");
 //                            xpp.next();
 //                            buffer.append(xpp.getText());//category 요소의 TEXT 읽어와서 문자열버퍼에 추가
 //                            buffer.append("\n");//줄바꿈 문자 추가
-//                        }
-//                        else if(tag.equals("defCnt")){
+//                        } else if (tag.equals("defCnt")) {
 //                            buffer.append("확진자 수 :");
 //                            xpp.next();
 //                            buffer.append(xpp.getText());//description 요소의 TEXT 읽어와서 문자열버퍼에 추가
 //                            buffer.append("\n");//줄바꿈 문자 추가
-//                        }
-//                        else if(tag.equals("gubun")){
+//                        } else if (tag.equals("gubun")) {
 //                            buffer.append("시도 :");
 //                            xpp.next();
 //                            buffer.append(xpp.getText());//telephone 요소의 TEXT 읽어와서 문자열버퍼에 추가
 //                            buffer.append("\n");//줄바꿈 문자 추가
-//                        }
-//                        else if(tag.equals("incDec")){
+//                        } else if (tag.equals("incDec")) {
 //                            buffer.append("전일대비 증감 수  :");
 //                            xpp.next();
 //                            buffer.append(xpp.getText());//address 요소의 TEXT 읽어와서 문자열버퍼에 추가
 //                            buffer.append("\n");//줄바꿈 문자 추가
-//                        }
-//                        else if(tag.equals("isolㅣClearCnt")){
+//                        } else if (tag.equals("isolClearCnt")) {
 //                            buffer.append("격리 해제 수   :");
 //                            xpp.next();
 //                            buffer.append(xpp.getText());//mapx 요소의 TEXT 읽어와서 문자열버퍼에 추가
 //                            buffer.append(" \n "); //줄바꿈 문자 추가
-//                        }
-//                        else if(tag.equals("isollngCnt")){
+//                        } else if (tag.equals("isolIngCnt")) {
 //                            buffer.append("격리중 환자수 :");
 //                            xpp.next();
 //                            buffer.append(xpp.getText());//mapy 요소의 TEXT 읽어와서 문자열버퍼에 추가
 //                            buffer.append("\n"); //줄바꿈 문자 추가
-//                        }
-//                        else if(tag.equals("localOccCnt")){
+//                        } else if (tag.equals("localOccCnt")) {
 //                            buffer.append("지역발생 수  :");
 //                            xpp.next();
 //                            buffer.append(xpp.getText());//mapy 요소의 TEXT 읽어와서 문자열버퍼에 추가
 //                            buffer.append("\n"); //줄바꿈 문자 추가
-//                        }
-//                        else if(tag.equals("localOccCnt")){
+//                        } else if (tag.equals("overFlowCnt")) {
 //                            buffer.append("해외 유입 수 :");
 //                            xpp.next();
 //                            buffer.append(xpp.getText());//mapy 요소의 TEXT 읽어와서 문자열버퍼에 추가
 //                            buffer.append("\n"); //줄바꿈 문자 추가
-//                        }
-//
-//                        else if(tag.equals("seq")){
+//                        } else if (tag.equals("seq")) {
 //                            buffer.append("게시글번호  :");
 //                            xpp.next();
 //                            buffer.append(xpp.getText());//mapy 요소의 TEXT 읽어와서 문자열버퍼에 추가
 //                            buffer.append("\n"); //줄바꿈 문자 추가
-//                        }
-//                        else if(tag.equals("stdDay")){
+//                        } else if (tag.equals("stdDay")) {
 //                            buffer.append("기준일시  :");
 //                            xpp.next();
 //                            buffer.append(xpp.getText());//mapy 요소의 TEXT 읽어와서 문자열버퍼에 추가
@@ -181,13 +207,13 @@
 //                        break;
 //
 //                    case XmlPullParser.END_TAG:
-//                        tag= xpp.getName(); //테그 이름 얻어오기
+//                        tag = xpp.getName(); //테그 이름 얻어오기
 //
-//                        if(tag.equals("item")) buffer.append("\n");// 첫번째 검색결과종료..줄바꿈
+//                        if (tag.equals("item")) buffer.append("\n");// 첫번째 검색결과종료..줄바꿈
 //                        break;
 //                }
 //
-//                eventType= xpp.next();
+//                eventType = xpp.next();
 //            }
 //
 //        } catch (Exception e) {
@@ -199,4 +225,22 @@
 //
 //    }
 //
+//
 //}
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
